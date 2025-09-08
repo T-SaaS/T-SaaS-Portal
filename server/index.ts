7e862f3a0c4569c7d930c3a0226d617aa8836b45
stefanoMejia <jhemejia95@gmail.com>
2025-09-07 19:32:15 -0500

    Fix CORS origin for TSASS portal in server configuration

--- server/index.ts
+++ server/index.ts
@@ -38,8 +38,8 @@ const corsOptions = {
       // Add your production domain here when deployed
       "https://driverqualificationtool.onrender.com",
       "https://driverqualificationtool.onrender.com/",
-      "http://tsassportal.vlocitycapital.com",
-      "http://tsassportal.vlocitycapital.com/",
+      "http://tsaasportal.vlocitycapital.com",
+      "http://tsaasportal.vlocitycapital.com/",
     ];
 
     // Add origins from environment variable
